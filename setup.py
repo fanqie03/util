d4c931020ad052816fc3266b3267a37c640292fd
mengfu188 <1242733702@qq.com>
2020-02-21 13:42:42 +0800

    init commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+# coding:utf-8
+import setuptools
+from setuptools import setup
+
+setup(
+    name='dl',
+    version='0.0.0',
+    author='',
+    author_email='',
+    description='',
+    long_description='',
+    license='MIT',
+    url='',
+    packages=setuptools.find_packages(),
+    include_package_data=True,  # 启用清单文件MANIFEST.in
+    install_requires=[
+    ],
+    classifiers=[
+        'Topic :: Utilities',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6"
+    ]
+)
